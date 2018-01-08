3412957c1620a9f353060de1a820afdfa2cb6c3a
Andy Fu <f255431@gmail.com>
2018-01-08 14:50:02 +0800

    Clean up the codes.

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-var filteredArray = _.uniq(['Andy', 1, 'Andy', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-// console.log(_.isString(true));
-// console.log(_.isString('Andy'));
-
-// console.log('Result: ', notes.add(10, -8));
-
-// var user = os.userInfo();
-
-// fs.appendFile('UserInfo.txt', `User name: ${user.username}, you are: ${notes.age} \n`);
