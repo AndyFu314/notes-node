c5b5b764ee85fd71cba7ca468fb2e75875269df7
Andy Fu <f255431@gmail.com>
2018-01-09 17:33:36 +0800

    Refactoring for reusability.

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ console.log('Command: ', command);
 console.log('Yargs', argv);
 
 if (command === 'add'){
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+    if(_.isUndefined(note)){
+        console.log('Added fail, the title may duplicate.');
+    }else{
+        console.log('Note added.');
+        console.log('--');
+        console.log("Title: ", note.title);
+        console.log(`Body: ${note.body}`);
+    }
 }else if (command === 'list'){
     notes.getAll();
 }else if (command === 'read'){
